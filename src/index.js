bec1eab7efc2631f7814a37db53874a5bd79fd0f
David <wjw3298@gmail.com>
2023-08-09 16:15:36 -0400

    complete w/o last advanced deliverables

--- src/index.js
+++ src/index.js
@@ -4,22 +4,36 @@ const ramenName = document.querySelector('#ramen-detail h2')
 const ramenRestaurant = document.querySelector('#ramen-detail h3')
 const rating = document.querySelector('#rating-display')
 const comments = document.querySelector('#comment-display')
+const inputForm = document.querySelector('#new-ramen')
+const updateForm = document.getElementById('edit-ramen')
+
 
 fetch('http://localhost:3000/ramens')
 .then(response => response.json())
 .then(ramenList => {
-    ramenList.forEach(ramen => {
-        showRamenImage(ramen)
-    })
+    ramenList.forEach(ramen => showRamenImage(ramen))
     ramenDetails(ramenList[0])
 })
 
 function showRamenImage(ramen){
+        const newDiv = document.createElement('div')
         const imageDisplay = document.createElement('img')
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = "Delete"
+
+        newDiv.append(imageDisplay, deleteButton)
+
         imageDisplay.src = ramen.image
-        ramenImageDisplay.appendChild(imageDisplay)
+        ramenImageDisplay.appendChild(newDiv)
         imageDisplay.addEventListener('click', () => ramenDetails(ramen))
+        deleteButton.addEventListener('click', () => {
+            imageDisplay.parentElement.remove(ramen)
+        })
+        console.log(imageDisplay.parentElement)
 }
+
+
+
 
 function ramenDetails(ramen){
     displayImage.src = ramen.image
@@ -29,8 +43,6 @@ function ramenDetails(ramen){
     comments.textContent = ramen.comment
 }
 
-const inputForm = document.querySelector('#new-ramen')
-
 inputForm.addEventListener('submit', e => {
     e.preventDefault()
     const newName = document.querySelectorAll('#new-ramen input')[0].value
@@ -38,8 +50,6 @@ inputForm.addEventListener('submit', e => {
     const newImage = document.querySelectorAll('#new-ramen input')[2].value
     const newRating = document.querySelectorAll('#new-ramen input')[3].value
     const newComment = document.querySelectorAll('#new-ramen textarea')[0].value
-    
-
     let newRamen = {
         name: newName,
         restaurant: newRestaurant,
@@ -49,3 +59,12 @@ inputForm.addEventListener('submit', e => {
     }
     showRamenImage(newRamen)
 })
+
+updateForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const ratingEdit = document.querySelectorAll('#edit-ramen input')[0].value
+    const commentEdit = document.querySelectorAll('#edit-ramen textarea')[0].value
+    rating.textContent = ratingEdit
+    comments.textContent = commentEdit
+})
+
